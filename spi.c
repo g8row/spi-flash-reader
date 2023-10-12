6cdaba86f4c8cbe4e713c67a0ddea66edeecc5a5
g8row <g8row@github.com>
2023-10-12 17:12:15 +0300

    add comments to spi.c

--- spi.c
+++ spi.c
@@ -14,14 +14,14 @@ static void pabort(const char *s)
 }
 int main()
 {
-	int dev = open("/dev/spidev0.0", O_RDWR);
-	int file = open("spictest.bin", O_RDWR| O_TRUNC | O_CREAT , S_IRWXU);
+	int dev = open("/dev/spidev0.0", O_RDWR); //open file descriptor to the spi device
+	int file = open("spictest.bin", O_RDWR| O_TRUNC | O_CREAT , S_IRWXU); // open a file in the current folder, delete previous content, create a new file if it doenst exist, set permissions
 	
 	if (dev < 0)
 		pabort("can't open device");
 	int ret;
 	int mode = SPI_MODE_1;	
-	ret = ioctl(dev, SPI_IOC_WR_MODE, &mode);
+	ret = ioctl(dev, SPI_IOC_WR_MODE, &mode); // set and get spi parameters
 	if (ret == -1)	
 		pabort("can't set spi mode");
 	ret = ioctl(dev, SPI_IOC_RD_MODE, &mode);
@@ -42,14 +42,14 @@ int main()
 	if(ret == -1)
 		pabort("can't get max speed");
 
-	int sector = 1024;	
-	u_int8_t tx[4+1024];
+	int sector = 1024; // set how much bytes to read in one transfer
+	u_int8_t tx[4+1024]; // create array with 4 bytes for the command + address and [sector] bytes for reading
         u_int8_t rx[4+1024] = {0, };
 	
-	u_int8_t cmd = 0x03;
+	u_int8_t cmd = 0x03; // what command to execute (Read data)
 	int address;	
 	
-        struct spi_ioc_transfer tr = {
+        struct spi_ioc_transfer tr = { // fill the struct with the arrays, length of the transfer, delay, speed and bits per word
                	.tx_buf = (unsigned long)tx,
                	.rx_buf = (unsigned long)rx,
                	.len = 4+sector,
@@ -58,22 +58,22 @@ int main()
                	.bits_per_word = bits,
        	};
 
-	int writes=1024*(4096/sector);
+	int transfers=1024*(4096/sector); // calculate how many transfers to do
 	
-	for(int i=0;i<writes;i++){
-		address = i*sector;	
-		tx[0] = cmd;
-		tx[1] = (address >> 16) & 0b11111111;
+	for(int i=0;i<transfers;i++){
+		address = i*sector; // which address to start reading from	
+		tx[0] = cmd; // set first byte as the command
+		tx[1] = (address >> 16) & 0b11111111; // split address to the next 3 bytes
 		tx[2] = (address >> 8) & 0b11111111;
 		tx[3] = address & 0b11111111;
 		
-        	ret = ioctl(dev, SPI_IOC_MESSAGE(1), &tr);
+        	ret = ioctl(dev, SPI_IOC_MESSAGE(1), &tr); // run the transfer using the struct
         	if (ret < 1)
                 	pabort("can't send spi message");
 
-		lseek(file,0,SEEK_END);
-		write(file,&(rx[4]),sector);
+		lseek(file,0,SEEK_END); // move the file pointer to the end of the output file
+		write(file,&(rx[4]),sector); // write from the 5th byte of the rx array, the first for are from the writing of the command
 	}
-	close(dev);
+	close(dev);// close both files
 	close(file);
 }
